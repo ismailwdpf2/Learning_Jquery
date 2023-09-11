9cae282bfad51148f846678f986961f7a5de0a8e
ismail hosen <ismailwdpf@gmail.com>
2023-09-11 21:19:28 +0600

    video

--- part5/main.js
+++ part5/main.js
@@ -1,12 +1,33 @@
 $(document).ready(function () {
-  $("#hide").click(function(){
-$("p.hs").hide('slow'); //slow/fast/5000/2000/ digit mean miliseconds
+  $(".btn1").click(function () {
+    $("#animate1").animate({
+      // left: '100px',
+      // height: '150px',
+      // width: '150px'
+      left: "100px",
+      height: "+=150px",
+      width: "+=150px",
+    });
   });
-  $("#show").click(function(){
-    $("p.hs").show('slow'); //slow/fast/5000/2000/ digit mean miliseconds
+  $(".btn2").click(function () {
+    $("#animate2").animate({
+      left: "100px",
+      height: "-=150px",
+      width: "-=150px",
+    });
   });
-
-  $("button.tgl").click(function(){
-    $('p.tgl').toggle('slow');
-  })
+  $(".btn3").click(function () {
+    var isamil = $("#animate3");
+    isamil.animate({ height: "300px" }, "slow");
+    isamil.animate({ width: "300px" }, "slow");
+    isamil.animate({ height: "100px" }, "slow");
+    isamil.animate({ width: "100px" }, "slow");
+  });
+  $(".btn4").click(function () {
+    var isamil = $("#animate4");
+    isamil.animate({ left: "100px"}, "slow");
+    isamil.animate({ height: "200px" }, "slow");
+    isamil.animate({ width: "300px" }, "slow");
+    isamil.animate({ fontSize:"30px" }, "first");
+  });
 });
