b9422e6dea3271a98f082f62dba9fd1ad28fe1d4
ismail hosen <ismailwdpf@gmail.com>
2023-10-09 18:09:15 +0600

    part 13

---
+++ part13/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+$('#btn1').click(function(){
+   $('#lorem1').text("hi");
+})
+
+$("#btn2").click(function(){
+   $("#lorem2").html("<h2>hello</h2>");
+})
+$("#btn3").click(function(){
+   $("#lorem3").val("enter your name");
+})
+});
