754d86ae07121652676a8502f507b55eb96815e7
ismail hosen <ismailwdpf@gmail.com>
2023-10-10 09:06:55 +0600

    part 16

---
+++ part16/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+$("#btn").click(function(){
+ $("p").addClass('ismail hosen'); //2 ta class add hoise
+})
+
+$('#btn2').click(function(){
+   $('.hasan').removeClass();   //class remove hobe
+})
+});
